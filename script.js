810f3547a4c46290e49da24323baedc573896534
Dmitrynest2012 <53977888+Dmitrynest2012@users.noreply.github.com>
2024-02-21 10:32:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -450,8 +450,8 @@ function readExcelToJson(url) {
 
 
 // URL файлов Excel
-const excelUrlMin = 'https://raw.githubusercontent.com/Dmitrynest2012/message-of-love/main/json_min.xlsx';
-const excelUrlMax = 'https://raw.githubusercontent.com/Dmitrynest2012/message-of-love/main/json_max.xlsx';
+const excelUrlMin = 'https://github.com/Dmitrynest2012/message-of-love/raw/main/json_min.xlsx';
+const excelUrlMax = 'https://github.com/Dmitrynest2012/message-of-love/raw/main/json_min.xlsx';
 
 
 // Загружаем и читаем файлы Excel, сохраняем данные в переменные json_min и json_max

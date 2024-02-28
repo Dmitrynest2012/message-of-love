e6329a9b94c0113822e807c52c7939e0ae89d31f
Dmitrynest2012 <53977888+Dmitrynest2012@users.noreply.github.com>
2024-02-28 19:22:53 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -485,6 +485,7 @@ const imageLinks = [
   'https://github.com/Dmitrynest2012/message-of-love/raw/main/message-random-3.jpg',
   'https://github.com/Dmitrynest2012/message-of-love/raw/main/message-random-4.png',
   'https://github.com/Dmitrynest2012/message-of-love/raw/main/message-random-5.jpg',
+  'https://github.com/Dmitrynest2012/message-of-love/raw/main/message-random-6.jpg',
   // Добавьте здесь остальные ссылки на картинки
 ];
 
